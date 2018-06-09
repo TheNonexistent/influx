4ee3a3be6012943985a16deb8a007004ae6f3ef1
TheNonexistent <TheNonexistent@users.noreply.github.com>
2018-06-10 04:00:12 +0430

    Update SampleModule.py

--- Documents/Samples/SampleModule.py
+++ Documents/Samples/SampleModule.py
@@ -27,3 +27,9 @@ def influx_module_main(prams):
 if __name__ == "__main__":
     print("This File Is An Influx-Module Created To Be Imported In The Influx Core And Function Within It.")
     print("It Is Not Meant To Run On It's Own. You Can Import This Module To A Matching Influx Version.")
+
+	
+###################################################################
+#Many Programs Need Files To Function. For Using Files And Directories For Your Influx-Module You Can-
+#-Use The Directory (infpath/ModDb), And Create A Directory For Your Influx-Module. There Soon Will Be A Mechanism To Manage Such Needs.
+###################################################################
